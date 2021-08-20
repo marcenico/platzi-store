c84437ed7aed80f3880eaaa5e01c76da73f67aa4
Marcelo Puebla <marcenp23@gmail.com>
2021-08-20 18:13:57 -0300

    Instalado module flexboxgrid

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -5,10 +5,11 @@ import { ProductComponent } from './components/product/product.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductsRoutingModule } from './components/products-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [ProductsComponent, ProductComponent, ProductDetailComponent],
   exports: [ProductsComponent, ProductComponent, ProductDetailComponent],
-  imports: [CommonModule, ProductsRoutingModule, SharedModule],
+  imports: [CommonModule, ProductsRoutingModule, SharedModule, MaterialModule],
 })
 export class ProductsModule {}
